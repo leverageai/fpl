558d6220c4d712ef4734f6feeba3c5ad96995d66
JonNixonCodes <jonathan.yu.94@gmail.com>
2024-07-06 18:54:58 +1000

    Created dim views

---
+++ dbt/fpl/models/dim_fixture.sql
@@ -0,0 +1,10 @@
+SELECT
+  id AS fixture_id,
+  event AS gameweek_id,
+  team_h AS home_team_id,
+  team_a AS away_team_id,
+  kickoff_time,
+FROM
+  `leverageai-sandbox.source.ext_src_fpl_api_fixtures`
+WHERE
+  source_date=(SELECT MAX(source_date) FROM `leverageai-sandbox.source.ext_src_fpl_api_fixtures`)
